6f4411fa9a2defe98fd2246ca4c8608c44c7429b
fomak <kandy.foma@sasol.com>
2025-04-02 18:05:36 +0200

    Remove CNAME file, update package-lock.json to standardize project name, and adjust vite.config.ts for Vercel deployment

--- africanite/vite.config.ts
+++ africanite/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/africanite-services/',
+  base: '/', // Set to '/' for Vercel deployment
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
